ca2d53938bcad697d3f2909c738bfd51314ebd1a
임정현 <chfhddl1234@naver.com>
2023-10-23 17:10:15 +0900

    :hammer: refactor : 생성자, getter 어노테이션으로 대체

--- backend/src/main/java/com/npc/say_vr/domain/study/constant/StudyResponseMessage.java
+++ backend/src/main/java/com/npc/say_vr/domain/study/constant/StudyResponseMessage.java
@@ -1,15 +1,13 @@
 package com.npc.say_vr.domain.study.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum StudyResponseMessage {
     STUDY_CREATE_SUCCESS("스터디 생성 완료");
 
     private final String message;
 
-    StudyResponseMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
 }
--- backend/src/main/java/com/npc/say_vr/domain/user/constant/UserExceptionMessage.java
+++ backend/src/main/java/com/npc/say_vr/domain/user/constant/UserExceptionMessage.java
@@ -1,15 +1,13 @@
 package com.npc.say_vr.domain.user.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserExceptionMessage {
     USER_NOT_FOUND("존재하지 않는 유저입니다");
 
     private final String message;
 
-    UserExceptionMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
 }
