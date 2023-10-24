23cfc751762fbeaab81080d72417135c3a3a85c2
Jongwon <jkjongwon@gmail.com>
2023-10-24 15:57:08 +0900

    :sparkles: feat(flashcards): 기본 단어(한/영 뜻) 엔티티 작성

---
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Word.java
@@ -0,0 +1,26 @@
+package com.npc.say_vr.domain.flashcards.domain;
+
+import com.npc.say_vr.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Word extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "word_id")
+    private Long id;
+    private String english;
+    private String korean;
+
+    private String voice;
+}
