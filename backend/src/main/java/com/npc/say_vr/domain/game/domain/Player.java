2eab2ea45a27b560282611932e1a5f3a63c406ae
임정현 <chfhddl1234@naver.com>
2023-10-24 13:21:12 +0900

    :heavy_plus_sign: feat(game) : Player 엔티티 추가

---
+++ backend/src/main/java/com/npc/say_vr/domain/game/domain/Player.java
@@ -0,0 +1,43 @@
+package com.npc.say_vr.domain.game.domain;
+
+import com.npc.say_vr.domain.game.constant.PlayerStatus;
+import com.npc.say_vr.domain.user.domain.User;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    private int winCnt;
+
+    @Enumerated(value = EnumType.STRING)
+    private PlayerStatus status;
+
+}
