b70e1c379815e6ec9cfdeb4506bbc95048e76454
suchshin <scshin97@gmail.com>
2023-10-24 13:37:13 +0900

    :heavy_plus_sign: feat(mystudyanalysispage): 내 학습 기록 분석 페이지

--- frontend/src/pages/MyStudyAnalysisPage/index.tsx
+++ frontend/src/pages/MyStudyAnalysisPage/index.tsx
@@ -1,10 +1,91 @@
-function MyStudyAnalysisPage() {
+import React, { useState, useEffect } from "react";
+import Calendar, { CalendarProps } from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import ApexCharts from "react-apexcharts";
+import AverageScore from "../../assets/MygradeAssets/AverageScore.png";
+import ContextScore from "../../assets/MygradeAssets/ContextScore.png";
+import GrammarScore from "../../assets/MygradeAssets/GrammarScore.png";
+import ProunciationScore from "../../assets/MygradeAssets/PronunciationScore.png";
+import "./style.css";
 
-    return (
-      <div>
-        <h1>내 학습 기록 분석</h1>
+type MyCalendarValue = Date | undefined;
+
+function MyStudyAnalysisPage() {
+  const [value, onChange] = useState<MyCalendarValue>(new Date());
+
+  const handleChange = (date: MyCalendarValue) => {
+    onChange(date);
+  };
+
+  const chartOptions = {
+    series: [
+      {
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "line" as const,
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"],
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
+    },
+  };
+
+  useEffect(() => {
+    const chartElement = document.querySelector("#chart") as HTMLElement;
+    const chart = new ApexCharts(chartElement, chartOptions);
+    chart.render();
+
+    // Cleanup the chart when the component unmounts
+    return () => {
+      if (chartElement && chartElement.parentNode) {
+        chartElement.parentNode.removeChild(chartElement);
+      }
+    };
+  }, []); // Empty dependency array to run the effect only once
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-5 custom-chart-container" style={{ marginRight: "30px" }}>
+          <h3>학습 달력📆</h3>
+          <Calendar onChange={handleChange as CalendarProps["onChange"]} value={value} className="calendar" />
+        </div>
+        <div className="col-sm-6">
+          <div className="row">
+            <div className="col-8 col-sm-12 custom-chart-container" style={{ marginBottom: "30px" }}>
+              <h3>내 현재 등급</h3>
+              <img src={AverageScore} alt="badgeimg" className="scorebadge" />
+              <img src={ContextScore} alt="badgeimg" className="scorebadge" />
+              <img src={GrammarScore} alt="badgeimg" className="scorebadge" />
+              <img src={ProunciationScore} alt="badgeimg" className="scorebadge" />
+            </div>
+            <div className="col-4 col-sm-12 custom-chart-container" style={{ marginBottom: "20px" }}>
+            <h3>학습별 평균 등급 변동치</h3>
+            <ApexCharts options={chartOptions as any} series={chartOptions.series} type="line" height={250} />
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default MyStudyAnalysisPage;
+    </div>
+  );
+}
+
+export default MyStudyAnalysisPage;
