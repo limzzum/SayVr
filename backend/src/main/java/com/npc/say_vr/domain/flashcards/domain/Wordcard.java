e5a7605156f2365fa813fcc43941343a6ec6478a
Jongwon <jkjongwon@gmail.com>
2023-10-24 16:02:08 +0900

    :sparkles: feat(flashcards): 단어장 속 단어Wordcard 엔티티 작성 학습된, 복습필요한, 삭제된 enum status 추가

---
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Wordcard.java
@@ -0,0 +1,35 @@
+package com.npc.say_vr.domain.flashcards.domain;
+
+import com.npc.say_vr.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Wordcard extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wordcard_id")
+    private Long id;
+    
+    private WordcardStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "word_id")
+    private Word word;
+
+    @ManyToOne
+    @JoinColumn(name = "flashcards_id")
+    private Flashcards flashcards;
+
+}
---
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/WordcardStatus.java
@@ -0,0 +1,7 @@
+package com.npc.say_vr.domain.flashcards.domain;
+
+public enum WordcardStatus {
+    CHECKED,
+    UNCHECKED,
+    DELETED
+}
