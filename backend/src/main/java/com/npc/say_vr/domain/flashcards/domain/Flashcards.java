931f3ccfe1cba0d7ec0d1599643fb68414ddb095
Jongwon <jkjongwon@gmail.com>
2023-10-24 13:39:17 +0900

    :sparkles: feat(flashcards): 단어장 기본 엔티티 작성

--- backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Flashcards.java
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Flashcards.java
@@ -2,10 +2,13 @@ package com.npc.say_vr.domain.flashcards.domain;
 
 import com.npc.say_vr.global.entity.BaseEntity;
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +16,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
 public class Flashcards extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "flashcards")
+    @Column(name = "flashcards_id")
     private Long id;
+    private String name;
 
 }
