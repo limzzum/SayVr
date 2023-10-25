bb2aae923b42e5f5739adb070fa3ad8539cf9fe4
Jongwon <jkjongwon@gmail.com>
2023-10-25 09:27:22 +0900

    :hammer: refactor(flashcards): 단어장엔티티 개인단어장 일대일 관계로 매핑 수정

--- backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Flashcards.java
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Flashcards.java
@@ -3,14 +3,12 @@ package com.npc.say_vr.domain.flashcards.domain;
 import com.npc.say_vr.global.entity.BaseEntity;
 import java.util.List;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,8 +16,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn
 public class Flashcards extends BaseEntity {
 
     @Id
@@ -30,5 +26,8 @@ public class Flashcards extends BaseEntity {
 
     @OneToMany(mappedBy = "flashcards")
     private List<Wordcard> wordcards;
+
+    @OneToOne(mappedBy = "flashcards")
+    private PersonalFlashcards personalFlashcards;
 
 }
