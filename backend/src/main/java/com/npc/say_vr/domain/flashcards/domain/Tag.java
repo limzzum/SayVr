d46c89d6fa682ce6904bd54ff5129140d7401f6d
Jongwon <jkjongwon@gmail.com>
2023-10-25 11:16:20 +0900

    :heavy_plus_sign: feat(flashcards): tag 시스템 별도 테이블로 관리 decktag 조인테이블

---
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Tag.java
@@ -0,0 +1,23 @@
+package com.npc.say_vr.domain.flashcards.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String content;
+}
