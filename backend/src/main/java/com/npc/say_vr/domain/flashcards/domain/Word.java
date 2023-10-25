bc03fe20ce5009f629fd093c024cc314094109b4
Jongwon <jkjongwon@gmail.com>
2023-10-25 13:44:04 +0900

    :hammer: refactor(flashcards): word 기본요소에 BaseEntity 적용

--- backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Word.java
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/domain/Word.java
@@ -1,5 +1,6 @@
 package com.npc.say_vr.domain.flashcards.domain;
 
+import com.npc.say_vr.global.entity.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Word {
+public class Word extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
