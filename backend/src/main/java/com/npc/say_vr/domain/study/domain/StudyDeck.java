3ca200c29479c148d5829f9808a3e7afbaa284de
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-10-26 13:58:23 +0900

    :heavy_plus_sign: Feat(study) : 엔티티 수정

--- backend/src/main/java/com/npc/say_vr/domain/study/domain/StudyDeck.java
+++ backend/src/main/java/com/npc/say_vr/domain/study/domain/StudyDeck.java
@@ -1,11 +1,20 @@
 package com.npc.say_vr.domain.study.domain;
 
 
+import com.npc.say_vr.domain.flashcards.domain.FlashcardDeck;
+import com.npc.say_vr.domain.user.domain.User;
+import com.npc.say_vr.global.constant.Status;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,5 +28,16 @@ public class StudyDeck {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "studyDeck_id")
   private Long id;
+
+  @Enumerated(EnumType.STRING)
+  private Status status;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "study_id")
+  private Study study;
+
+  @OneToOne
+  @JoinColumn(name = "flashcards_id")
+  private FlashcardDeck flashcardDeck;
 
 }
