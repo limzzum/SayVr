4253158b900273b3d71782f328b1d1e2beb47406
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-10-26 18:06:39 +0900

    :sparkles: Feat(study) : STUDY 생성중 API

--- backend/src/main/java/com/npc/say_vr/domain/study/api/StudyApiController.java
+++ backend/src/main/java/com/npc/say_vr/domain/study/api/StudyApiController.java
@@ -1,10 +1,14 @@
 package com.npc.say_vr.domain.study.api;
 
+import com.npc.say_vr.domain.study.dto.StudyRequestDto.CreateStudyRequestDto;
+import com.npc.say_vr.domain.study.service.StudyService;
 import com.npc.say_vr.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/study")
 public class StudyApiController {
 
+  private final StudyService studyService;
+
+  // TODO
+  @PostMapping("")
+  public ResponseEntity<?> createStudy(@RequestBody CreateStudyRequestDto createStudyRequestDto) {
+    Long userId = 1L;
+//    return ResponseEntity.ok(studyService.createStudy(userId,createStudyRequestDto), HttpStatus.)
+    return null;
+  }
 }
