374d808edf8ef6a9c35ac0d5b24bcebee1998ec7
임정현 <chfhddl1234@naver.com>
2023-10-27 17:46:20 +0900

    :sparkles: feat : 티어에 따른 대기열 등록 서비스 구현

--- backend/src/main/java/com/npc/say_vr/domain/game/service/GameServiceImpl.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/service/GameServiceImpl.java
@@ -1,7 +1,10 @@
 package com.npc.say_vr.domain.game.service;
 
+import com.npc.say_vr.domain.game.domain.Ranking;
+import com.npc.say_vr.domain.game.repository.RankingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,5 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class GameServiceImpl implements GameService {
 
+    private static final String EXCAHGE_NAME = "sample.exchange";
+    private final RabbitTemplate rabbitTemplate;
+    private final RankingRepository rankingRepository;
 
+
+
+    @Override
+    public void registWaitingQueue(Long userId) {
+        Ranking ranking = rankingRepository.findByUserId(userId).orElseThrow();
+        String name = ranking.getTier().getName();
+        rabbitTemplate.convertAndSend(EXCAHGE_NAME, name, name+"queue 등록");
+    }
 }
