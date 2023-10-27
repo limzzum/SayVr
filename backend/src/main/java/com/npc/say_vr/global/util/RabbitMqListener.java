b4cdc637c0896d7991ea779e6d255dee24552b60
임정현 <chfhddl1234@naver.com>
2023-10-27 17:44:53 +0900

    :sparkles: feat : rabbitmq 리스너 추가

--- backend/src/main/java/com/npc/say_vr/global/util/RabbitMqListener.java
+++ backend/src/main/java/com/npc/say_vr/global/util/RabbitMqListener.java
@@ -1,15 +1,48 @@
 package com.npc.say_vr.global.util;
 
+import com.rabbitmq.client.Channel;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class RabbitMqListener {
+
     @RabbitListener(queues = "sample.queue")
     public void receiveMessage(final Message message) {
         log.info(message.toString());
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+        value = @Queue(value = "bronze.queue", durable = "true"),
+        exchange = @Exchange(value = "amp.direct"),
+        key = "bronze"
+    ))
+    public void bronze(Message message, Channel channel) throws IOException {
+
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+        value = @Queue(value = "silver.queue", durable = "true"),
+        exchange = @Exchange(value = "amp.direct"),
+        key = "silver"
+    ))
+    public void silver(Message message, Channel channel) throws IOException {
+
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+        value = @Queue(value = "gold.queue", durable = "true"),
+        exchange = @Exchange(value = "amp.direct"),
+        key = "gold"
+    ))
+    public void gold(Message message, Channel channel) throws IOException {
+
     }
 }
