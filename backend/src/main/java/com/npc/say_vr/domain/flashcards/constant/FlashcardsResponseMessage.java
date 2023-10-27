1cb33a919856ce7a11c93cb732eeef7210c32b11
Jongwon <jkjongwon@gmail.com>
2023-10-27 16:21:12 +0900

    :hammer: refactor(flashcards): 메세지 작성 (생성)

--- backend/src/main/java/com/npc/say_vr/domain/flashcards/constant/FlashcardsResponseMessage.java
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/constant/FlashcardsResponseMessage.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum FlashcardsResponseMessage {
-    SUCCESS_CREATE("단어장 생성 성공");
+    SUCCESS_CREATE_DECK("단어장 생성 성공"),
+    SUCCESS_CREATE_WORD("단어 추가 성공");
 
     private final String message;
 
