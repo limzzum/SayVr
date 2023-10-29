45baee5ffe925d255051ef5480fd8806ad62c26b
limzzum <chfhddl1234@naver.com>
2023-10-30 03:12:53 +0900

    :sparkles: feat : 게임 소켓 요청 dto 추가

--- backend/src/main/java/com/npc/say_vr/domain/game/dto/GameRequestDto.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/dto/GameRequestDto.java
@@ -1,5 +1,6 @@
 package com.npc.say_vr.domain.game.dto;
 
+import com.npc.say_vr.domain.game.constant.SocketType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,5 +14,14 @@ public class GameRequestDto {
     @AllArgsConstructor
     public static class GameStartRequestDto {
         private Long userId;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GameSocketRequestDto {
+        private SocketType socketType;
+        private String text;
     }
 }
