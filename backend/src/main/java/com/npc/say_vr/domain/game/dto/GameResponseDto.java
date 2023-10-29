960c343623f5155e7bdf49466cf28c8223e37862
limzzum <chfhddl1234@naver.com>
2023-10-30 03:22:15 +0900

    :sparkles: feat : 게임 소켓 응답 dto 추가

--- backend/src/main/java/com/npc/say_vr/domain/game/dto/GameResponseDto.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/dto/GameResponseDto.java
@@ -1,5 +1,7 @@
 package com.npc.say_vr.domain.game.dto;
 
+import com.npc.say_vr.domain.game.constant.SocketType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -18,13 +20,22 @@ public class GameResponseDto {
 
     @Getter
     public static class GameWaitingResponseDto {
-
         private Long gameId;
 
         @Builder
         public GameWaitingResponseDto(Long gameId) {
             this.gameId = gameId;
         }
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class GameSocketResponseDto {
+        private SocketType socketType;
+        private GameStatusDto gameStatusDto;
+        private String text;
+
     }
 
 }
