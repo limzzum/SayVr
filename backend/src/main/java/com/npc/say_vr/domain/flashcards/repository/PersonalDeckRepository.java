950308f97254704644b96ce822766418c74ed2e7
Jongwon <jkjongwon@gmail.com>
2023-10-29 22:06:56 +0900

    :hammer: refactor(flashcards): 단어장 검색, 태그 리포지토리 작업중

--- backend/src/main/java/com/npc/say_vr/domain/flashcards/repository/PersonalDeckRepository.java
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/repository/PersonalDeckRepository.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonalDeckRepository extends JpaRepository<PersonalDeck, Long> {
-    
+
     List<PersonalDeck> findByUser_Id(Long userId);
+
+    List<PersonalDeck> findByNameContaining(String keyword);
+
+//    List<PersonalDeck> findByTagsContainingAndTags(List<DeckTag> tags);
 
 
 }
