2516a421c33e740f9b15fa7d5db71ab15469c9a9
limzzum <chfhddl1234@naver.com>
2023-10-30 02:48:53 +0900

    :sparkles: feat : 게임 대기 응답 메시지 추가

--- backend/src/main/java/com/npc/say_vr/domain/game/constant/GameResponseMessage.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/constant/GameResponseMessage.java
@@ -2,12 +2,14 @@ package com.npc.say_vr.domain.game.constant;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 @Getter
 public enum GameResponseMessage {
-    GAME_START_SUCCESS("게임 시작 완료");
+    GAME_WAITING_SUCCESS("게임 대기 등록 완료", HttpStatus.OK);
 
     private final String message;
+    private final HttpStatus status;
 
 }
