440ebef562bb80954947e8e0ab64aa9c397afdcc
Jongwon <jkjongwon@gmail.com>
2023-10-29 22:30:17 +0900

    :heavy_plus_sign: feat(vr): 대화 DTO, gpt api 형식에 맞춤

---
+++ backend/src/main/java/com/npc/say_vr/domain/vr/domain/Message.java
@@ -0,0 +1,40 @@
+package com.npc.say_vr.domain.vr.domain;
+
+import com.npc.say_vr.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Message extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "talk_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conversation_id")
+    private Conversation conversation;
+
+    //TODO: confirm
+    private String role;// system or user
+    private String content; // 대사
+
+    @Range(min = 0, max = 100)
+    private Integer grammar;
+    @Range(min = 0, max = 100)
+    private Integer context;
+    @Range(min = 0, max = 100)
+    private Integer pronunciation;
+}
