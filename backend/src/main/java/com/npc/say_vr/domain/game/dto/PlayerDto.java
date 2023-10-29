272ded328d711dfc3fdf0423c887a404c959c7c2
limzzum <chfhddl1234@naver.com>
2023-10-30 02:57:40 +0900

    :sparkles: feat : player dto 추가

---
+++ backend/src/main/java/com/npc/say_vr/domain/game/dto/PlayerDto.java
@@ -0,0 +1,20 @@
+package com.npc.say_vr.domain.game.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerDto {
+
+    private Long userId;
+    private Long ranking;
+    private Long point;
+    private int winCnt;
+    private String profile;
+
+}
