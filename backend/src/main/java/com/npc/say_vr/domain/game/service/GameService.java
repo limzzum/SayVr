491cacdbeff7e1292300359c53019ccaeb606ec1
임정현 <chfhddl1234@naver.com>
2023-10-30 17:47:59 +0900

    :sparkles: feat(game) : 퀴즈 관련 서비스 추가

--- backend/src/main/java/com/npc/say_vr/domain/game/service/GameService.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/service/GameService.java
@@ -3,5 +3,9 @@ package com.npc.say_vr.domain.game.service;
 public interface GameService {
     Long create();
     Long registWaitingQueue(Long userId);
+    boolean checkQuizAnswer(Long gameId, String answer);
+    String getQuizQuestion(String answer);
+    String createQuizAnswer();
+
 
 }
