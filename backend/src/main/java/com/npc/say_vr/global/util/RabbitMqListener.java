719743c52ed74389a5d1a0e35dee4817352fbc0e
임정현 <chfhddl1234@naver.com>
2023-10-30 16:48:17 +0900

    :sparkles: feat(game) : 소켓 disconnection 이벤트 리스너 추가

--- backend/src/main/java/com/npc/say_vr/global/util/RabbitMqListener.java
+++ backend/src/main/java/com/npc/say_vr/global/util/RabbitMqListener.java
@@ -1,52 +1,26 @@
 package com.npc.say_vr.global.util;
 
-import com.rabbitmq.client.Channel;
-import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 @Slf4j
 public class RabbitMqListener {
 
-    @RabbitListener(bindings = @QueueBinding(
-        value = @Queue(value = "bronze.queue", durable = "true"),
-        exchange = @Exchange(value = "amp.direct"),
-        key = "bronze"
-    ))
-    public void bronze(Message message, Channel channel) throws IOException {
-        log.info(message.toString());
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-        value = @Queue(value = "silver.queue", durable = "true"),
-        exchange = @Exchange(value = "amp.direct"),
-        key = "silver"
-    ))
-    public void silver(Message message, Channel channel) throws IOException {
-
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-        value = @Queue(value = "gold.queue", durable = "true"),
-        exchange = @Exchange(value = "amp.direct"),
-        key = "gold"
-    ))
-    public void gold(Message message, Channel channel) throws IOException {
-
+    @EventListener
+    public void connectionListener(SessionConnectedEvent event){
+        log.info("connected");
+        String name = event.getUser().getName();
+        System.out.println("userId " + name);
     }
 
     @EventListener
-    public void connectionListener(SessionConnectedEvent event){
+    public void connectionListener(SessionDisconnectEvent event){
+        log.info("disconnected");
         String name = event.getUser().getName();
-        System.out.println("name " + name);
-        System.out.println("source " + event.getSource());
+        System.out.println("userId " + name);
     }
 }
