5f87616adf0a1b5e4d66d42b99d7a4678113e671
임정현 <chfhddl1234@naver.com>
2023-10-31 16:16:25 +0900

    :sparkles: feat(game) : 레디스 게임 상태 value 모두 가져오는 메서드 추가

--- backend/src/main/java/com/npc/say_vr/global/util/RedisUtil.java
+++ backend/src/main/java/com/npc/say_vr/global/util/RedisUtil.java
@@ -1,19 +1,22 @@
 package com.npc.say_vr.global.util;
 
+import com.npc.say_vr.domain.game.dto.GameStatusDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
-
 @Component
 @RequiredArgsConstructor
 public class RedisUtil {
 
     private final RedisTemplate<String, Object> redisTemplate;
-    private final RedisTemplate<String, Object> redisGameStatusTemplate;
+    private final RedisTemplate<String, GameStatusDto> redisGameStatusTemplate;
     private final RedisTemplate<String, Object> redisBlackListTemplate;
 
     @Value("${jwt.expmin}")
@@ -50,24 +53,34 @@ public class RedisUtil {
     }
 
     public boolean hasKeyExcludeList(String key) {
-        return Boolean.TRUE.equals(redisGameStatusTemplate.hasKey(key));
+        return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
     }
 
-    public void setGameStatusList(String key, Object o) {
-        redisBlackListTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(o.getClass()));
-        redisTemplate.opsForValue().set(key, o, 30 * 1000 * 60, TimeUnit.MILLISECONDS);
+    public void setGameStatusList(String key, GameStatusDto gameStatusDto) {
+        redisGameStatusTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(gameStatusDto.getClass()));
+        redisGameStatusTemplate.opsForValue().set(key, gameStatusDto, 30 * 1000 * 60, TimeUnit.MILLISECONDS);
     }
 
-    public Object getGameStatusList(String key) {
-        return redisBlackListTemplate.opsForValue().get(key);
+    public GameStatusDto getGameStatusList(String key) {
+        return (GameStatusDto) redisGameStatusTemplate.opsForValue().get(key);
     }
 
     public boolean deleteGameStatusList(String key) {
-        return Boolean.TRUE.equals(redisBlackListTemplate.delete(key));
+        return Boolean.TRUE.equals(redisGameStatusTemplate.delete(key));
     }
 
     public boolean hasKeyGameStatusList(String key) {
-        return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
+        return Boolean.TRUE.equals(redisGameStatusTemplate.hasKey(key));
+    }
+
+    public List<GameStatusDto> getGameStatusValues() {
+        List<GameStatusDto> gameStatusDtos = new ArrayList<>();
+        Set<String> keys = redisGameStatusTemplate.keys("*");
+        for (String key : keys) {
+            GameStatusDto gameStatusDto = redisGameStatusTemplate.opsForValue().get(key);
+            gameStatusDtos.add(gameStatusDto);
+        }
+        return gameStatusDtos;
     }
 
 
