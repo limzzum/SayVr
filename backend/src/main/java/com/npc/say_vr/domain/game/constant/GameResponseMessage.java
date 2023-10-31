5543d474a9b9b1dd4bfb4163b1a79cccc25af6c6
임정현 <chfhddl1234@naver.com>
2023-10-31 16:13:36 +0900

    :sparkles: feat(game) : 게임 응답 메시지 추가

--- backend/src/main/java/com/npc/say_vr/domain/game/constant/GameResponseMessage.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/constant/GameResponseMessage.java
@@ -10,7 +10,8 @@ public enum GameResponseMessage {
     GAME_WAITING_SUCCESS("게임 대기 등록 완료", HttpStatus.OK),
     GAME_IS_END("게임 종료", HttpStatus.OK),
     IS_ANSWER("정답입니다", null),
-    IS_BAD_ANSWER("틀렸습니다", null);
+    IS_BAD_ANSWER("틀렸습니다", null),
+    PLAYER_OUT_MESSAGE("상대방이 게임을 이탈하여 게임을 종료합니다.", null);
 
     private final String message;
     private final HttpStatus status;
