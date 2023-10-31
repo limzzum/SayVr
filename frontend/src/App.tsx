7bbd5842e1bb910d3cce9f7f512f4f8a516fb722
suchshin <scshin97@gmail.com>
2023-10-31 16:21:25 +0900

    :heavy_plus_sign: feat(shadowingpage): 유튜브 API 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,11 @@ import MyStudyAnalysisPage from "./pages/MyStudyAnalysisPage";
 import ShadowingPage from "./pages/ShadowingPage";
 import StudyPage from "./pages/StudyPage";
 import VocabListPage from "./pages/VocabListPage";
+import BBCPage from "./pages/ShadowingPage/BBCPage"
+import CNNPage from "./pages/ShadowingPage/CNNPage";
+import TeamCOCOPage from "./pages/ShadowingPage/TeamCOCOPage";
+import TEDPage from "./pages/ShadowingPage/TEDPage"; 
+
 function App() {
   return (
     <Routes>
@@ -17,6 +22,10 @@ function App() {
       <Route path="/My" element={<MyPage />} />
       <Route path="/MyStudyAnalysis" element={<MyStudyAnalysisPage />} />
       <Route path="/Shadowing" element={<ShadowingPage />} />
+      <Route path="/Shadowing/BBCPage" element={<BBCPage />} />
+      <Route path="/Shadowing/CNNPage" element={<CNNPage />} />
+      <Route path="/Shadowing/TeamCOCOPage" element={<TeamCOCOPage />} />
+      <Route path="/Shadowing/TEDPage" element={<TEDPage />} />
       <Route path="/Study" element={<StudyPage />} />
       <Route path="/VocabList" element={<VocabListPage />} />
     </Routes>
