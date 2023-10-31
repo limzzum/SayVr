d1b38fbe139d3c33b86f7a392c9b2c451919d080
임정현 <chfhddl1234@naver.com>
2023-10-31 16:16:41 +0900

    :sparkles: feat(game) : 사용자 나가기 소켓 타입 추가

--- backend/src/main/java/com/npc/say_vr/domain/game/constant/SocketType.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/constant/SocketType.java
@@ -1,5 +1,5 @@
 package com.npc.say_vr.domain.game.constant;
 
 public enum SocketType {
-    GAME_INFO, GAME_END, CHAT, QUIZ, QUIZ_TIME_OVER
+    GAME_INFO, GAME_END, CHAT, QUIZ, QUIZ_TIME_OVER, PLAYER_OUT
 }
