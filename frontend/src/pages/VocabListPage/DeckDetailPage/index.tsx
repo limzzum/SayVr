a05b654e292589594606db320d2ee28f1c95c3da
Jongwon <jkjongwon@gmail.com>
2023-11-02 17:36:33 +0900

    :sparkles: feat(flashcards): 단독 목록 페이지,단어장 상세페이지

---
+++ frontend/src/pages/VocabListPage/DeckDetailPage/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function DeckDetailPage() {
+  return (
+    <>
+      <div className='container mt-5'>
+        <div className='vocab-list-container row card-row justify-content-center align-items-center '>
+          <div className='row justify-content-center align-items-center'></div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default DeckDetailPage
