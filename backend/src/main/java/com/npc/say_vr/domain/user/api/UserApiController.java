99b83b993ef74018c214cd2781a7f45a32900011
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-11-03 15:34:03 +0900

    :sparkles: feat(user) : facebook로그인 추가

--- backend/src/main/java/com/npc/say_vr/domain/user/api/UserApiController.java
+++ backend/src/main/java/com/npc/say_vr/domain/user/api/UserApiController.java
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -31,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
