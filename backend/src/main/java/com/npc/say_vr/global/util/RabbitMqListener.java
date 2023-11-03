56998128727a686638e7611a9505108bc6a41381
임정현 <chfhddl1234@naver.com>
2023-11-03 17:34:11 +0900

    :bug: fix(game) : 리스너 오류 임시 주석 처리

--- backend/src/main/java/com/npc/say_vr/global/util/RabbitMqListener.java
+++ backend/src/main/java/com/npc/say_vr/global/util/RabbitMqListener.java
@@ -30,38 +30,38 @@ public class RabbitMqListener {
     private final GameService gameService;
     private static final String EXCAHGE_NAME = "amq.topic";
 
-    @RabbitListener(bindings = @QueueBinding(
-        value = @Queue(value = "game.queue", durable = "true"),
-        exchange = @Exchange(value = EXCAHGE_NAME),
-        key = "alarm.*"
-    ))
-    public void bronze(Message message, Channel channel) throws IOException {
-        //TODO: 알람만 받도록 해야함
-        if(!message.getMessageProperties().getReceivedRoutingKey().equals("alarm.*")){
-            System.out.println("알람 아님");
-            System.out.println(message.toString());
-            return;
-        }
-        byte[] body = message.getBody();
-        String gameId = new String(body);
-        log.info("{}번 게임방 퀴즈 제한시간 종료 메시지큐 리스너 호출됨 ", gameId);
-
-        GameSocketResponseDto gameSocketResponseDto;
-        if(gameService.isEndGame(Long.valueOf(gameId))){
-            gameSocketResponseDto = GameSocketResponseDto.builder().socketType(SocketType.GAME_END)
-                .data(gameService.getGameResult(Long.valueOf(gameId)))
-                .build();
-            rabbitTemplate.convertAndSend(EXCAHGE_NAME, "game." + gameId, gameSocketResponseDto);
-            return;
-        }
-
-        String quiz = gameService.updateQuiz(Long.valueOf(gameId));
-        gameSocketResponseDto = GameSocketResponseDto.builder().socketType(SocketType.QUIZ_TIME_OVER)
-            .message(quiz)
-            .build();
-        rabbitTemplate.convertAndSend(EXCAHGE_NAME, "game." + gameId, gameSocketResponseDto);
-
-    }
+//    @RabbitListener(bindings = @QueueBinding(
+//        value = @Queue(value = "game.queue", durable = "true"),
+//        exchange = @Exchange(value = EXCAHGE_NAME),
+//        key = "alarm.*"
+//    ))
+//    public void bronze(Message message, Channel channel) throws IOException {
+//        //TODO: 알람만 받도록 해야함
+//        if(!message.getMessageProperties().getReceivedRoutingKey().equals("alarm.*")){
+//            System.out.println("알람 아님");
+//            System.out.println(message.toString());
+//            return;
+//        }
+//        byte[] body = message.getBody();
+//        String gameId = new String(body);
+//        log.info("{}번 게임방 퀴즈 제한시간 종료 메시지큐 리스너 호출됨 ", gameId);
+//
+//        GameSocketResponseDto gameSocketResponseDto;
+//        if(gameService.isEndGame(Long.valueOf(gameId))){
+//            gameSocketResponseDto = GameSocketResponseDto.builder().socketType(SocketType.GAME_END)
+//                .data(gameService.getGameResult(Long.valueOf(gameId)))
+//                .build();
+//            rabbitTemplate.convertAndSend(EXCAHGE_NAME, "game." + gameId, gameSocketResponseDto);
+//            return;
+//        }
+//
+//        String quiz = gameService.updateQuiz(Long.valueOf(gameId));
+//        gameSocketResponseDto = GameSocketResponseDto.builder().socketType(SocketType.QUIZ_TIME_OVER)
+//            .message(quiz)
+//            .build();
+//        rabbitTemplate.convertAndSend(EXCAHGE_NAME, "game." + gameId, gameSocketResponseDto);
+//
+//    }
 
     @EventListener
     public void connectionListener(SessionConnectedEvent event){
