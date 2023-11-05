4c6d68857cadab2c902fcf8ebc8c7e9991536cd9
limzzum <chfhddl1234@naver.com>
2023-11-06 02:21:38 +0900

    :sparkles: feat(game) : Tier Builder 추가

--- backend/src/main/java/com/npc/say_vr/domain/game/domain/Tier.java
+++ backend/src/main/java/com/npc/say_vr/domain/game/domain/Tier.java
@@ -6,12 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Tier {
 
     @Id
