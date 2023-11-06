f8acc552e4daacef8841449cef78fee042f39c9b
임정현 <chfhddl1234@naver.com>
2023-11-06 11:30:13 +0900

    :bug: fix(game) : ssl 인증된 호스트로 요청

--- frontend/src/pages/MatchingGamePage/constants/constants.tsx
+++ frontend/src/pages/MatchingGamePage/constants/constants.tsx
@@ -1,2 +1,2 @@
 export const socketURL: string = "https://xn--vr-kg0j662g.site/api/ws";
-export const serverURL: string = "https://k9a501.p.ssafy.io";
+export const serverURL: string = "https://xn--vr-kg0j662g.site";
