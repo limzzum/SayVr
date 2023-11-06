591c51065f6ac3859da67256d27301ed94cde025
Jongwon <jkjongwon@gmail.com>
2023-11-06 13:00:17 +0900

    :hammer: refactor(flashcards): CORS *

--- backend/src/main/java/com/npc/say_vr/domain/flashcards/api/WordcardApiController.java
+++ backend/src/main/java/com/npc/say_vr/domain/flashcards/api/WordcardApiController.java
@@ -13,6 +13,7 @@ import com.npc.say_vr.domain.flashcards.service.WordcardService;
 import com.npc.say_vr.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/flashcards")
