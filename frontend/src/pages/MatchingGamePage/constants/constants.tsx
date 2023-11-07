a778f23e444ce10cb19f5d485f79952dc6d5aefb
임정현 <chfhddl1234@naver.com>
2023-11-07 17:45:33 +0900

    :sparkles: feat(game) : 서버 배포 url로 변경

--- frontend/src/pages/MatchingGamePage/constants/constants.tsx
+++ frontend/src/pages/MatchingGamePage/constants/constants.tsx
@@ -1,4 +1,4 @@
-export const socketURL: string = "http://localhost:8080/api/ws";
+export const socketURL: string = "https://xn--vr-kg0j662g.site/api/ws";
 export const subscribeURL: string = "/topic/game";
-export const serverURL: string = "http://localhost:8080";//"https://xn--vr-kg0j662g.site";
+export const serverURL: string = "https://xn--vr-kg0j662g.site";
 export const imageURL: string = "https://xn--vr-kg0j662g.site/profiles/";
