0bc7b8a1960ef9b2381f3fb3b0d28626e831363e
suchshin <scshin97@gmail.com>
2023-11-07 16:43:24 +0900

    임시커밋

--- frontend/src/api/MyStudyAnalysisAPI/GetMyAverageScore.tsx
+++ frontend/src/api/MyStudyAnalysisAPI/GetMyAverageScore.tsx
@@ -1,19 +1,21 @@
-import axios from 'axios';
-import API_URL from "../../../config"
-// const getMyAverageScore = async (token) => {
-    const getMyAverageScore = async () => {
+import axios from "axios";
+import API_URL from "../../config";
+// const GetMyAverageScore = async (token) => {
+const GetMyAverageScore = async () => {
   try {
-    const response = await axios.get(`${API_URL}/virtual/score`, {
-      headers: {
-        Authorization: null,
-      },
+    const response = await axios.get(`${API_URL}/conversation/score`, {
+      //   headers: {
+      //     Authorization: null,
+      //   },
     });
+    console.log("나의 점수");
+    console.log(response);
 
     return response.data;
   } catch (error) {
-    console.error('Error fetching average score:', error);
-    throw error; 
+    console.error("Error fetching average score:", error);
+    throw error;
   }
 };
 
-export default getMyAverageScore;
+export default GetMyAverageScore;
