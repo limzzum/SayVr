d25ca6980d1b7c324d5ef3f4ebf764322669da7a
suchshin <scshin97@gmail.com>
2023-11-07 13:51:08 +0900

    :heavy_plus_sign: feat(MyStudyAnalysisDetailPage): 내 학습 기록 페이지 구현중

--- frontend/src/pages/MyStudyAnalysisPage/MyStudyAnalysisDetailPage/index.tsx
+++ frontend/src/pages/MyStudyAnalysisPage/MyStudyAnalysisDetailPage/index.tsx
@@ -1,42 +1,40 @@
 import React, { useState } from "react";
+import GetConversation from "../../../api/MyStudyAnalysisAPI/GetConversation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
 function MyStudyAnalysisDetailPage() {
-  // 선택된 아이템을 state로 관리
   const [selectedItem, setSelectedItem] = useState("Item 1");
 
   return (
     <div className="container-fluid">
       <div className="row">
-        {/* 왼쪽 70% */}
         <div className="col-md-8">
           <div id="list-example" className="list-group">
-            {/* 가로로 배치된 a 태그들로 변경 */}
             <div className="d-flex">
               <a
                 className={`list-group-item list-group-item-action ${selectedItem === "Item 1" && "active"}`}
                 onClick={() => setSelectedItem("Item 1")}
               >
-                Item 1
+                1
               </a>
               <a
                 className={`list-group-item list-group-item-action ${selectedItem === "Item 2" && "active"}`}
                 onClick={() => setSelectedItem("Item 2")}
               >
-                Item 2
+                2
               </a>
               <a
                 className={`list-group-item list-group-item-action ${selectedItem === "Item 3" && "active"}`}
                 onClick={() => setSelectedItem("Item 3")}
               >
-                Item 3
+                3
               </a>
               <a
                 className={`list-group-item list-group-item-action ${selectedItem === "Item 4" && "active"}`}
                 onClick={() => setSelectedItem("Item 4")}
               >
-                Item 4
+                4
               </a>
             </div>
           </div>
@@ -47,28 +45,26 @@ function MyStudyAnalysisDetailPage() {
             className="scrollspy-example"
             tabIndex={0}
           >
-            {/* 각 섹션의 스타일을 수정 */}
-            <div style={{ display: selectedItem === "Item 1" ? "block" : "none" }}>
-              <h4 id="list-item-1">Item 1</h4>
+            <div style={{ display: selectedItem === "1" ? "block" : "none" }}>
+              <h4 id="list-item-1">1</h4>
               <p>...</p>
             </div>
-            <div style={{ display: selectedItem === "Item 2" ? "block" : "none" }}>
-              <h4 id="list-item-2">Item 2</h4>
+            <div style={{ display: selectedItem === "2" ? "block" : "none" }}>
+              <h4 id="list-item-2">2</h4>
               <p>...</p>
             </div>
-            <div style={{ display: selectedItem === "Item 3" ? "block" : "none" }}>
-              <h4 id="list-item-3">Item 3</h4>
+            <div style={{ display: selectedItem === "3" ? "block" : "none" }}>
+              <h4 id="list-item-3">3</h4>
               <p>...</p>
             </div>
-            <div style={{ display: selectedItem === "Item 4" ? "block" : "none" }}>
-              <h4 id="list-item-4">Item 4</h4>
+            <div style={{ display: selectedItem === "4" ? "block" : "none" }}>
+              <h4 id="list-item-4">4</h4>
               <p>...</p>
             </div>
           </div>
         </div>
-        {/* 오른쪽 30% */}
         <div className="col-md-4">
-          <div className="script-container">{/* 오른쪽에 텍스트를 넣어주세요 */}</div>
+          <div className="script-container">hello</div>
         </div>
       </div>
     </div>
