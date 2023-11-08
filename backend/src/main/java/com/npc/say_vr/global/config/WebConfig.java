80446ccc35317c64eb6d8bc239b432e48e4ade8e
jiwon <wldnjs9515@naver.com>
2023-11-09 00:51:24 +0900

    :construction_worker: setting : cors preflight 해결

---
+++ backend/src/main/java/com/npc/say_vr/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.npc.say_vr.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://k9a501.p.ssafy.io", "https://k9a501.p.ssafy.io")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
+
+
