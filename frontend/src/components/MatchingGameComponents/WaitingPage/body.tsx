46bcf4badd79cfccff97a691f76407d105b46be9
limzzum <chfhddl1234@naver.com>
2023-11-13 10:30:45 +0900

    :sparkles: feat(game) : 매칭 화면 상대방 프로필 추가

--- frontend/src/components/MatchingGameComponents/WaitingPage/body.tsx
+++ frontend/src/components/MatchingGameComponents/WaitingPage/body.tsx
@@ -1,18 +1,33 @@
 import { useRef } from "react";
 import "./style.css";
 
-
 interface props {
-    image : string
+  image: string;
+  rankPoint1: number;
+  opponent?: string;
+  rankPoint2?: number;
 }
-const Body: React.FC<props> = ({image}) => {
-  return (
-    <div className="body-container">
-        <div className="profile-container"></div>        
-        <img src= {image} alt="profile image" className="profile-image"/>
-        
-    </div>
-  );
-}
+const Body: React.FC<props> = ({ image, rankPoint1, opponent, rankPoint2 }) => {
+  if (opponent == null) {
+    return (
+      <div className="body-container">
+        <div className="profile-container"></div>
+        {/* <div> rankPoint1</div> */}
+        <img src={image} alt="profile image" className="profile-image" />
+      </div>
+    );
+  } else {
+    return (
+      <div className="body-container">
+        <div className="profile-container"></div>
+        {/* <div> rankPoint1</div> */}
+        <img src={image} alt="profile image" className="profile-image" />
+        <div className="profile-container"></div>
+        {/* <div> rankPoint1</div> */}
+        <img src={opponent} alt="profile image" className="profile-image2" />
+      </div>
+    );
+  }
+};
 
 export default Body;
