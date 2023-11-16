8bb3aacc8eec62f0e40d873ebd3c1c1207077e60
suchshin <scshin97@gmail.com>
2023-11-16 11:18:23 +0900

    :heavy_plus_sign: feat(font): 글꼴 적용과 단어추가 기능 구현중

--- frontend/src/components/MyStudyAnalysisComponents/Translation/Translation.tsx
+++ frontend/src/components/MyStudyAnalysisComponents/Translation/Translation.tsx
@@ -1,18 +1,65 @@
-// Translation.tsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios, { AxiosRequestConfig } from "axios";
+import { Modal, Button } from "react-bootstrap";
+import API_URL from "../../../config";
+import { tokenState } from "../../../recoil/GoalbalState";
+import { useRecoilValue } from "recoil";
 import "./style.css";
 
 const endpoint = "https://api.cognitive.microsofttranslator.com";
 const location = "eastus";
 
 interface TranslationProps {
-  // 필요한 프롭스 추가
+  // 필요한 프로퍼티 추가
 }
 
 const Translation: React.FC<TranslationProps> = () => {
   const [textToTranslate, setTextToTranslate] = useState<string>("");
   const [translatedText, setTranslatedText] = useState<string | null>(null);
+  const [userId, setUserId] = useState<number | null>(null);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [flashcards, setFlashcards] = useState<Array<{ name: string }>>([]);
+  const authToken = useRecoilValue(tokenState);
+
+  useEffect(() => {
+    setUserId(18);
+  }, []);
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const fetchFlashcards = async () => {
+    if (!userId) {
+      console.error("사용자 ID를 찾을 수 없습니다.");
+      return;
+    }
+
+    const flashcardRoute = `/flashcards/list`;
+    const flashcardConfig: AxiosRequestConfig = {
+      method: "get",
+      url: `${API_URL}${flashcardRoute}`,
+      headers: {
+        Authorization: authToken,
+      },
+      params: {
+        userId: userId,
+      },
+    };
+
+    try {
+      const flashcardResponse = await axios(flashcardConfig);
+      console.log("flashcardResponse", flashcardResponse);
+
+      const flashcardsData = flashcardResponse.data?.data?.personalDeckList || [];
+
+      setFlashcards(flashcardsData);
+
+      setShowModal(true);
+    } catch (error) {
+      console.error("단어장을 가져오는 중 오류가 발생했습니다:", error);
+    }
+  };
 
   const translate = async () => {
     const route = "/translate?api-version=3.0&from=en&to=ko";
@@ -34,6 +81,8 @@ const Translation: React.FC<TranslationProps> = () => {
       const response = await axios(config);
       const translatedText = response.data[0].translations[0].text;
       setTranslatedText(translatedText);
+
+      setShowModal(true);
     } catch (error) {
       console.error(error);
     }
@@ -46,7 +95,7 @@ const Translation: React.FC<TranslationProps> = () => {
           <textarea
             value={textToTranslate}
             onChange={(e) => setTextToTranslate(e.target.value)}
-            placeholder="Enter text to translate"
+            placeholder="번역할 텍스트를 입력하세요"
             className="text-box form-control"
           />
         </div>
@@ -54,15 +103,43 @@ const Translation: React.FC<TranslationProps> = () => {
       <div className="half-width-container">
         <div className="output-container">
           <div className="output-box">
-            <p>{translatedText || "Translated text will appear here"}</p>
+            <p>{translatedText || "번역된 텍스트가 여기에 나타납니다"}</p>
           </div>
         </div>
         <div className="button-container">
           <button className="btn btn-primary" onClick={translate}>
             번역
           </button>
+          <button className="btn btn-primary" onClick={fetchFlashcards}>
+            단어장 추가
+          </button>
         </div>
       </div>
+
+      {/* 모달 */}
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>단어장 목록</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            <strong>번역 결과:</strong> {translatedText || "없음"}
+          </p>
+          <p>
+            <strong>단어장 목록:</strong>
+          </p>
+          <ul>
+            {flashcards.map((flashcard, index) => (
+              <li key={index}>{flashcard.name}</li>
+            ))}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModalClose}>
+            닫기
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,17 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import NavBar from "./components/NaVBar";
 import Modal from "react-modal";
+import "./index.css";
 
 Modal.setAppElement("#root");
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <BrowserRouter>
-  <RecoilRoot>
-    <NavBar />
-    <App />
+    <RecoilRoot>
+      <NavBar />
+      <App />
     </RecoilRoot>
   </BrowserRouter>
 );
