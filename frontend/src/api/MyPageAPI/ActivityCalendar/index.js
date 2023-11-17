22509133510a2f91b4c1ad63874dd4c976577870
pH-Saem <phsaem98@gmail.com>
2023-11-17 18:29:07 +0900

    🎨 design(마이페이지): 잔디 CSS 수정

--- frontend/src/api/MyPageAPI/ActivityCalendar/index.js
+++ frontend/src/api/MyPageAPI/ActivityCalendar/index.js
@@ -2,76 +2,80 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import API_URL from "../../../config";
 import { tokenState } from "../../../recoil/GoalbalState";
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue } from "recoil";
 import { ActivityCalendar } from "activity-calendar-react";
 import "./style.css";
 
 function ActiveCalendarComponent() {
-  const [activityData, setActivityData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const token = useRecoilValue(tokenState)
-  
-  useEffect(() => {
-    const fetchActivityData = async () => {
-      try {
-        console.log("요청은 가나");
+	const [activityData, setActivityData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const token = useRecoilValue(tokenState);
 
-        const response = await axios.get(`${API_URL}/user/active`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });        
-        console.log("요청은 가나");
-        console.log("엑티브 캘린더",response);
-        setActivityData(response.data.data.activityDtoList);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching activity data:", error);
-        setIsLoading(false);
-      }
-    };
+	useEffect(() => {
+		const fetchActivityData = async () => {
+			try {
+				console.log("요청은 가나");
 
-    fetchActivityData();
-  }, [token]);
+				const response = await axios.get(`${API_URL}/user/active`, {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				});
+				console.log("요청은 가나");
+				console.log("엑티브 캘린더", response);
+				setActivityData(response.data.data.activityDtoList);
+				setIsLoading(false);
+			} catch (error) {
+				console.error("Error fetching activity data:", error);
+				setIsLoading(false);
+			}
+		};
 
-  function getColorForActivity(activityCount) {
-    console.log(`Activity Count: ${activityCount}`);
-    if (activityCount === 0) {
-      return "#dadada";
-    } else if (activityCount === 1) {
-      return "#0e4429";
-    } else if (activityCount === 2) {
-      return "#006d32";
-    } else if (activityCount === 3) {
-      return "#26a641";
-    } else if (activityCount >= 4) {
-      return "#39d353";
-    }
-  }
+		fetchActivityData();
+	}, [token]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+	function getColorForActivity(activityCount) {
+		console.log(`Activity Count: ${activityCount}`);
+		if (activityCount === 0) {
+			return "#dadada";
+		} else if (activityCount === 1) {
+			return "#0e4429";
+		} else if (activityCount === 2) {
+			return "#006d32";
+		} else if (activityCount === 3) {
+			return "#26a641";
+		} else if (activityCount >= 4) {
+			return "#39d353";
+		}
+	}
 
-  // axios로 가져온 데이터를 sampleData와 같은 형식으로 변환
-  const transformedData = activityData.map((item) => ({
-    day: item.whenDate,
-    activity: item.count,
-  }));
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
 
-  const colorCustomization = {
-    activity0: '#dadada',
-    activity1: '#0e4429',
-    activity2: '#006d32',
-    activity3: '#26a641',
-    activity4: '#39d353',
-  };
+	// axios로 가져온 데이터를 sampleData와 같은 형식으로 변환
+	const transformedData = activityData.map((item) => ({
+		day: item.whenDate,
+		activity: item.count,
+	}));
 
-  return (
-    <div className="calendar-container">
-      <ActivityCalendar sampleData={transformedData} colorCustomization={colorCustomization} showMonth={true} />
-    </div>
-  );
+	const colorCustomization = {
+		activity0: "#dadada",
+		activity4: "#0e4429",
+		activity3: "#006d32",
+		activity2: "#26a641",
+		activity1: "#39d353",
+	};
+
+	return (
+		<div className="calendar-container">
+			<ActivityCalendar
+				sampleData={transformedData}
+				colorCustomization={colorCustomization}
+				showMonth={true}
+			/>
+		</div>
+	);
 }
 
 export default ActiveCalendarComponent;
