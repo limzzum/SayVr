6c449ff423a21f05b7486e73005dd9bf8ff14fe0
limzzum <chfhddl1234@naver.com>
2023-11-18 18:46:07 +0900

    :sparkles: feat(game) : 매칭 후 나와 상대방 프로필 표시

--- frontend/src/components/MatchingGameComponents/WaitingPage/body.tsx
+++ frontend/src/components/MatchingGameComponents/WaitingPage/body.tsx
@@ -1,30 +1,44 @@
 import { useRef } from "react";
 import "./style.css";
+import { imageURL } from "../../../pages/MatchingGamePage/constants/constants";
 
 interface props {
-  image: string;
-  rankPoint1: number;
+  image?: string;
+  rankPoint1?: number;
+  name1?: string;
   opponent?: string;
   rankPoint2?: number;
+  name2?: string;
   isMatch: boolean;
 }
-const Body: React.FC<props> = ({ image, rankPoint1, opponent, rankPoint2, isMatch }) => {
+const Body: React.FC<props> = ({
+  image,
+  rankPoint1,
+  name1,
+  opponent,
+  rankPoint2,
+  name2,
+  isMatch,
+}) => {
+  console.log("opponent image : " + opponent);
   if (!isMatch) {
     return (
       <div className="body-container">
         <div className="profile-container"></div>
-        <img src={image} alt="profile image" className="profile-image" />
+        <img src={image} className="profile-image" />
       </div>
     );
   } else {
     return (
-      <div className="body-container">
-        <div className="profile-container"></div>
+      <div className="body-container2">
+        <div className="match-profile-container1">
+          <img src={image} alt="profile" className="match-profile-image1" />
+        </div>
+        <div className="vs-text"> VS </div>
+        <div className="match-profile-container2">
+          <img src={imageURL + opponent} className="match-profile-image2" />
+        </div>
         {/* <div> rankPoint1</div> */}
-        <img src={image} alt="profile image" className="profile-image" />
-        <div className="profile-container"></div>
-        {/* <div> rankPoint1</div> */}
-        <img src={opponent} alt="profile image" className="profile-image2" />
       </div>
     );
   }
