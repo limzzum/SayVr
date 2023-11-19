82ae7de79a184fc86be565ce69e7beade323ea47
limzzum <chfhddl1234@naver.com>
2023-11-20 00:44:40 +0900

    :sparkles: feat : 이미지 슬라이더 이미지 추가
    Former-commit-id: 42b7e0756a1b07300dc7ec5071a10e25bee5ed7f

--- frontend/src/components/MainPageComponents/VideoSlider.tsx
+++ frontend/src/components/MainPageComponents/VideoSlider.tsx
@@ -3,12 +3,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React, { useRef } from "react";
 
-// import HomeVideo from "../../assets/MainPageAssets/video/Home.gif";
-// import MoveHomeAndExitVideo from "../../assets/MainPageAssets/video/MoveHomeAndExit.gif";
-// import TalkVideo from "../../assets/MainPageAssets/video/Talk.gif";
-// import TalkDannyVideo from "../../assets/MainPageAssets/video/TalkDanny.gif";
-// import UIVideo from "../../assets/MainPageAssets/video/UI.gif";
-// import VeniceVideo from "../../assets/MainPageAssets/video/Venice.gif";
+import HomeVideo from "../../assets/MainPageAssets/video/Home.gif";
+import TalkVideo from "../../assets/MainPageAssets/video/Talk.gif";
 import "./style.css";
 
 const ImageSlider = () => {
@@ -38,18 +34,8 @@ const ImageSlider = () => {
   return (
     <div className="imageSlider-container">
       <Slider className="imageSlider" {...settings}>
-        {/* <img className="imageSlider-item" src={HomeVideo} alt="Image 1" />
-
+        <img className="imageSlider-item" src={HomeVideo} alt="Image 1" />
         <img className="imageSlider-item" src={TalkVideo} alt="Image 1" />
-        <img className="imageSlider-item" src={TalkDannyVideo} alt="Image 1" />
-        <img className="imageSlider-item" src={UIVideo} alt="Image 1" />
-        <img className="imageSlider-item" src={VeniceVideo} alt="Image 1" />
-
-        <img
-          className="imageSlider-item"
-          src={MoveHomeAndExitVideo}
-          alt="Image 1"
-        /> */}
       </Slider>
     </div>
   );
